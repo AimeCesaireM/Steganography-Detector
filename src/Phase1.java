038a78c286d023e9b501058443ee4ba8f5559e91
AhsanRandhawa <83049347+AhsanRandhawa@users.noreply.github.com>
2022-11-01 00:54:00 -0400

    Found Length

--- src/Phase1.java
+++ src/Phase1.java
@@ -7,21 +7,39 @@ import javax.imageio.ImageIO;
 public class Phase1 {
 
     public static void main(String[] args) throws Exception {
-        BufferedImage image = ImageIO.read(new File("hide_text.png"));
+        BufferedImage image = ImageIO.read(new File("src/hide_text.png"));
         int width = image.getWidth();
         int height = image.getHeight();
         System.out.println("Height: " + height + " Width: " + width);
         WritableRaster raster = image.getRaster();
-        int count = 0;
+        int count = 31;
+        long lengthOfData = 0;
+        String data = "";
+        int thisByte = 0;
+        int countedBits = 1;
+
+        System.out.println(lengthOfData(raster, width, height));
+    }
+
+    public static int lengthOfData(WritableRaster raster, int width, int height)
+    {
+        int count = 31;
+        int length = 0;
         for (int r = 0; r < height; r++) {
             for (int c = 0; c < width; c++) {
+
                 int[] pixels = raster.getPixel(c, r, (int[]) null);
-                if (count < 32) {
-                    System.out.print((pixels[0] & 1) + "" + (pixels[1] & 1) + "" + (pixels[2] & 1));
-                    count++;
+                for(int i = 0; i < 3; i++)
+                {
+                    if(count < 0)
+                        return length;
+
+                    int bitMask = (pixels[i] & 1) << count;
+                    length = length | bitMask;
+                    count--;
                 }
             }
         }
-        System.out.println("");
+        return -1;
     }
 }
