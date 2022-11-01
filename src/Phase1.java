a23e32af9698e5ef25c3236af468f395187f2c5f
AhsanRandhawa <83049347+AhsanRandhawa@users.noreply.github.com>
2022-11-01 01:40:31 -0400

    Decoded .txt

--- src/Phase1.java
+++ src/Phase1.java
@@ -12,13 +12,27 @@ public class Phase1 {
         int height = image.getHeight();
         System.out.println("Height: " + height + " Width: " + width);
         WritableRaster raster = image.getRaster();
-        int count = 31;
-        long lengthOfData = 0;
-        String data = "";
-        int thisByte = 0;
-        int countedBits = 1;
+        int length = lengthOfData(raster, width, height);
+        System.out.println(length);
+        String data = getData(length, raster, width, height);
+        System.out.println(data);
+        //testCode(raster, width, height);
 
-        System.out.println(lengthOfData(raster, width, height));
+    }
+
+    public static void testCode(WritableRaster raster, int width, int height)
+    {
+
+        int count = 0;
+        for (int r = 0; r < height; r++) {
+            for (int c = 0; c < width; c++) {
+                int[] pixels = raster.getPixel(c, r, (int[]) null);
+                if (count < 32){
+                    System.out.print((pixels[0] & 1) + "" +  (pixels[1] & 1) + "" +  (pixels[2] & 1));
+                    count ++;
+                }
+            }
+        }
     }
 
     public static int lengthOfData(WritableRaster raster, int width, int height)
@@ -41,5 +55,38 @@ public class Phase1 {
             }
         }
         return -1;
+    }
+
+    public static String getData(int length, WritableRaster raster,  int width, int height)
+    {
+        String data = "";
+        int r = 0;
+        int c = 10;
+        int[] pixels = raster.getPixel(c, r, (int[]) null);
+        int thisByte =  (pixels[2] & 1) << 7;
+        long countedBits = 2;
+        c++;
+        for (; r < height; r++) {
+            for (; c < width; c++) {
+
+                pixels = raster.getPixel(c, r, (int[]) null);
+                for(int i = 0; i < 3; i++)
+                {
+                    if(countedBits > 8L * length)
+                        return data;
+
+                    int bitMask = (pixels[i] & 1) << ((8 - (countedBits % 8)) %8);
+                    thisByte = thisByte | bitMask;
+                    if (countedBits % 8 == 0)
+                    {
+                        //System.out.println(thisByte);
+                        data += (char) thisByte;
+                        thisByte = 0;
+                    }
+                    countedBits++;
+                }
+            }
+        }
+        return data;
     }
 }
