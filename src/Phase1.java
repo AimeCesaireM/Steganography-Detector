86f1744dddf1b4a596cae253cc7ba13fe5b9e148
AhsanRandhawa <83049347+AhsanRandhawa@users.noreply.github.com>
2022-11-07 20:13:42 -0500

    Added Multiple Functionalities e.g TBLR BRG LSBs pixelChannels>3

--- src/Phase1.java
+++ src/Phase1.java
@@ -2,6 +2,7 @@
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.io.File;
+import java.util.Locale;
 import javax.imageio.ImageIO;
 
 public class Phase1 {
@@ -10,18 +11,21 @@ public class Phase1 {
     static int globalColIndex;
     static int globalPixelIndex;
     static int numberOfPixelChannels;
+    static boolean isLRTB;
+    static boolean isGRB;
+    static int leastSignificantBits;
 
     public static void main(String[] args) throws Exception {
 
-        if (args.length != 2) {
-            //error because we need two arguments
-            System.err.println("Usage: Phase1 [filename] [HiddenMessageType]");
+        if (args.length != 6) {
+            //error because we need six arguments
+            System.err.println("Usage: Phase1 [filename] [HiddenMessageType] [LRTBOrTBLR] [GRB] [pixelChannels] [leastSignificantBits]");
             System.err.println("HiddenMessageType: 'png' or 'text' or 'lw'");
             System.exit (1);
         }
         else if (args[0].equals("") || (!args[1].equals("text") && !args[1].equals("png") && !args[1].equals("lw"))) {
             //error because we need two arguments
-            System.err.println("Usage: decoder [filename] [HiddenMessageType]");
+            System.err.println("Usage: Phase1 [filename] [HiddenMessageType]");
             System.err.println("HiddenMessageType: 'png' or 'text' or 'lw'");
             System.exit(1);
         }
@@ -32,10 +36,15 @@ public class Phase1 {
         System.out.println("Height: " + height + " Width: " + width);
         WritableRaster raster = image.getRaster();
 
+        isLRTB = args[2].equals("LRTB");
+        isGRB = args[3].equals("GRB");
+        numberOfPixelChannels = Integer.parseInt(args[4]);
+        leastSignificantBits = Integer.parseInt(args[5]);
+
         globalRowIndex = 0;
         globalColIndex = 0;
         globalPixelIndex = 0;
-        numberOfPixelChannels = 3;
+
 
         int lengthOrHeightOfHidden = getNextIntFromImage(raster, width, height);
         System.out.println("Length/Height of hidden message: " + lengthOrHeightOfHidden);
@@ -67,12 +76,31 @@ public class Phase1 {
 
     public static int getNextByteFromImage(WritableRaster raster, int width, int height)
     {
+
         return getNextXBits(raster, width, height, 8);
     }
-
     public static int getNextXBits(WritableRaster raster, int width, int height, int xBits)
     {
-        xBits -= 1;
+        if(isLRTB)
+        {
+            if(isGRB)
+                return LRTBAndGRB(raster, width, height, xBits);
+
+            return LRTBAndBRG(raster, width, height, xBits);
+        }
+
+        //So it is TBLR
+        if(isGRB)
+            return TBLRAndGRB(raster, width, height, xBits);
+
+        return TBLRAndBRG(raster, width, height, xBits);
+
+    }
+
+
+    public static int LRTBAndGRB(WritableRaster raster, int width, int height, int xBits)
+    {
+        xBits -= leastSignificantBits;
         int returnValue = 0;
         for (; globalRowIndex < height; globalRowIndex++) {
             for (; globalColIndex < width; globalColIndex++) {
@@ -83,15 +111,97 @@ public class Phase1 {
                     if(xBits < 0)
                         return returnValue;
 
-                    int bitMask = (pixels[globalPixelIndex] & 1) << xBits;
+                    int bitMask = (pixels[globalPixelIndex] & getMasker(leastSignificantBits)) << xBits;
                     returnValue = returnValue | bitMask;
-                    xBits--;
+                    xBits -= leastSignificantBits;
                 }
                 globalPixelIndex = 0;
             }
             globalColIndex = 0;
         }
         System.err.println("getNextBits exited Unexpectedly");
+        return -1;
+    }
+
+    public static int TBLRAndGRB(WritableRaster raster, int width, int height, int xBits)
+    {
+        xBits -= leastSignificantBits;
+        int returnValue = 0;
+        for (; globalColIndex < width; globalColIndex++) {
+            for (; globalRowIndex < height; globalRowIndex++) {
+
+                int[] pixels = raster.getPixel(globalColIndex, globalRowIndex, (int[]) null);
+                for(; globalPixelIndex < numberOfPixelChannels; globalPixelIndex++)
+                {
+                    if(xBits < 0)
+                        return returnValue;
+
+                    int bitMask = (pixels[globalPixelIndex] & getMasker(leastSignificantBits)) << xBits;
+                    returnValue = returnValue | bitMask;
+                    xBits -= leastSignificantBits;
+                }
+                globalPixelIndex = 0;
+            }
+            globalRowIndex = 0;
+        }
+        System.err.println("getNextBits exited Unexpectedly");
+        return -1;
+    }
+
+    public static int TBLRAndBRG(WritableRaster raster, int width, int height, int xBits)
+    {
+        xBits -= leastSignificantBits;
+        int returnValue = 0;
+        for (; globalColIndex < width; globalColIndex++) {
+            for (; globalRowIndex < height; globalRowIndex++) {
+
+                int[] pixels = raster.getPixel(globalColIndex, globalRowIndex, (int[]) null);
+                for(; globalPixelIndex >= 0; globalPixelIndex--)
+                {
+                    if(xBits < 0)
+                        return returnValue;
+
+                    int bitMask = (pixels[globalPixelIndex] & getMasker(leastSignificantBits)) << xBits;
+                    returnValue = returnValue | bitMask;
+                    xBits -= leastSignificantBits;
+                }
+                globalPixelIndex = numberOfPixelChannels - 1;
+            }
+            globalRowIndex = 0;
+        }
+        System.err.println("getNextBits exited Unexpectedly");
+        return -1;
+    }
+    public static int LRTBAndBRG(WritableRaster raster, int width, int height, int xBits)
+    {
+        xBits -= leastSignificantBits;
+        int returnValue = 0;
+        for (; globalRowIndex < height; globalRowIndex++) {
+            for (; globalColIndex < width; globalColIndex++) {
+
+                int[] pixels = raster.getPixel(globalColIndex, globalRowIndex, (int[]) null);
+                for(; globalPixelIndex >= 0; globalPixelIndex--)
+                {
+                    if(xBits < 0)
+                        return returnValue;
+
+                    int bitMask = (pixels[globalPixelIndex] & getMasker(leastSignificantBits)) << xBits;
+                    returnValue = returnValue | bitMask;
+                    xBits -= leastSignificantBits;
+                }
+                globalPixelIndex = numberOfPixelChannels - 1;
+            }
+            globalColIndex = 0;
+        }
+        System.err.println("getNextBits exited Unexpectedly");
+        return -1;
+    }
+
+    public static int getMasker(int bits)
+    {
+        if (bits == 1) return 1;
+        if (bits == 2) return 3;
+        System.err.println("Unexpected bits given to getMasker");
         return -1;
     }
 
